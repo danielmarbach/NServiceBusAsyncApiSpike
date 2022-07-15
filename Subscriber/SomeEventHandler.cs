22b63300da432ebb8ca66f575a2499e354a9a729
Daniel Marbach <daniel.marbach@openplace.net>
2022-07-15 13:16:01 +0200

    Add logging

--- Subscriber/SomeEventHandler.cs
+++ Subscriber/SomeEventHandler.cs
@@ -1,10 +1,13 @@
 using NServiceBus;
+using NServiceBus.Logging;
 
 public class SomeEventHandler : IHandleMessages<SomeEvent>
 {
     public Task Handle(SomeEvent message, IMessageHandlerContext context)
     {
-        Console.WriteLine("Received Some Event");
+        Log.Debug("Received Some Event");
         return Task.CompletedTask;
     }
+
+    private static ILog Log = LogManager.GetLogger<SomeEventHandler>();
 }
